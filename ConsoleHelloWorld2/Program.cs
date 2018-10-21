49228eb09f8651f15458af999c3005057138367e
Bob Oberst <bob.oberst@prodigy.net>
2018-10-21 16:23:22 -0500

    First commit.

--- ConsoleHelloWorld2/Program.cs
+++ ConsoleHelloWorld2/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleHelloWorld2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Goodbye Cruel World!");
             Console.ReadLine();
         }
     }
